b2d6d10c9a167f4b0abbcc3ea1bfd23c08fbee91
Ron Kurr <kurr@kurron.org>
2015-12-06 03:28:25 +0000

    Added an image priming file

---
+++ scripts/prime-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker pull kurron/docker-0xdbe:latest
+docker pull kurron/docker-charles-proxy:latest
+docker pull kurron/docker-clion:latest
+docker pull kurron/docker-dbvisualizer:latest
+docker pull kurron/docker-emacs:latest
+docker pull kurron/docker-intellij:latest
+docker pull kurron/docker-logfaces-client:latest
+docker pull kurron/docker-pycharm:latest
+docker pull kurron/docker-rubymine:latest
+docker pull kurron/docker-webstorm:latest
